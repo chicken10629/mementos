280efdf47a0eff4eb7672ffc91239bf5d34b62c4
EC2 Default User <ec2-user@ip-172-31-37-25.eu-north-1.compute.internal>
2024-12-10 15:03:18 +0000

    [update]テーブルのリレーション、一意性の定義、画像データのカラム追加

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,2 +1,6 @@
 class Follow < ApplicationRecord
+  belongs_to :user
+  
+  validates :follow_user_id, uniqueness: {scope: :followed_user_id}
+  #フォローしたユーザーとフォローされたユーザーが唯一であるか？
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one_attached :image #アイコン画像のデータ
+  #テーブルの関連付け
+  has_many :posts, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :follows, dependent: :destroy
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: { message: "を入力してください。" }
