c61e091b98915f7ee6aa7f3b7979f93da5d94ac2
EC2 Default User <ec2-user@ip-172-31-4-201.ap-northeast-1.compute.internal>
2024-12-27 14:16:17 +0000

    seedファイルの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# テストデータを作成　
+
+#該当のメールがなければ以下の設定とメアドで登録する。重複を防ぐ。
+#fixturesにサンプル画像を入れておく　ファイル名が一致するようにしておく
+olivia = User.find_or_create_by!(email: "olivia@example.com") do |user|
+  user.name = "Olivia"
+  user.password = "password"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")
+end
+
+james = User.find_or_create_by!(email: "james@example.com") do |user|
+  user.name = "James"
+  user.password = "password"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")
+end
+
+lucas = User.find_or_create_by!(email: "lucas@example.com") do |user|
+  user.name = "Lucas"
+  user.password = "password"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")
+end
