7cf0ca20d5bb75a3ed01018f6ee06bf566c2ad7c
EC2 Default User <ec2-user@ip-172-31-4-201.ap-northeast-1.compute.internal>
2025-01-04 00:39:08 +0000

    [wip]いいねのアクション処理

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,11 +1,29 @@
 class Public::FavoritesController < ApplicationController
+  
+  #あるユーザーがいいねした一覧を表示したい。とりあえずcurrent_userのみでやる
+  #has_manyを設定しているとjoinを使えるらしい。
+  #joinは二つ以上のテーブルを関連付け、一度にデータを取得
+  #
   def index
+    @favorites = current_user.posts.joins(:favorites)
   end
 
   def create
+    #ビュー側でインスタンス変数使わなければローカルでよさげ？
+    #拾ってくるidはネストしてる親のid
+    #
+    post = Post.find(params[:post_id])
+    favorite = current_user.favorites.new(post_id: post.id)
+    favorite.save
+    redirect_to post_path(post)
   end
 
   def destroy
+    #ネストしているものは基本的に親のデータを取得し、各idをセットしてからデータをいじる。
+    post = Post.find(params[:post_id])
+    favorite = current_user.favorites.find_by(post_id: post.id)
+    favorite.destroy
+    redirect_to post_path(post)
   end
   
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,10 @@ class Post < ApplicationRecord
   has_one_attached :image
   validates :title, :body, presence: { message: "を入力してください。" }
   #あとでhas_many_attached :imagesに変更
+
+  #いいねがついているかのチェック　子ではなく親の方が機能するので、親側にメソッドを作る
+  def favorited_by?(user)
+    favorites/exists?(user_id: user.id)
+  end
+
 end
